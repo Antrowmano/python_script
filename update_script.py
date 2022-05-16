478db664a6b27e65fcfbd392b769d0ded1ef33af
antrow <antrowmano543@gmail.com>
2022-05-16 16:03:25 +0530

    adding update script

--- update_script.py
+++ update_script.py
@@ -1,42 +1,41 @@
-#!/usr/bin/python3
-
+!/opt/homebrew/bin/python3
+# import sys
 import pymysql
 import time
 import maskpass
-import functools
+
 def mysqlconnect():
-    host = input("Enter your host: ")
-    username = input("Enter your username: ")
-    password = maskpass.askpass(prompt="Enter your password: ", mask="*")
-    db_name= input("db name: ")
-    conn = pymysql.connect(
-        host = host,
-        user = username,
-        password = password,
-        db = db_name,
-        )
-    cur =conn.cursor()
-#    min_id=1
-    max_id=150
-    min_query=("select min(id) from audit_activity where updated_at is null;")
-    cur.execute(min_query)
-    output = cur.fetchone()
-    min_id = functools.reduce(lambda sub, ele: sub * 10 + ele, output)
-    print(min_id)
-    if min_id < max_id:
-        for i in range(min_id,max_id,5):
-            second_value=i+4
-            query=(f"update audit_activity set updated_at=ADDTIME(created_at,3)where updated_at is null and id BETWEEN {i} and {second_value};")
-            cur.execute(query)
-            conn.commit()
-            time.sleep(5)
-#           print(f'Done {second_value}')
-#        output = cur.fetchall()
-#        for i in output:
-#            print(i)
+    try:
+        host = input("Enter your host: ")
+        username = input("Enter your username: ")
+        password = maskpass.askpass(prompt="Enter your password: ", mask="*")
+        db_name= input("db name: ")
+        conn = pymysql.connect(
+            host = host,
+            user = username,
+            password = password,
+            db = db_name,
+            )
+        cur =conn.cursor()
+    #    min_id=1
+        max_id=5433907
+        min_query=("select min(id) from audit_activity where updated_at is null;")
+        cur.execute(min_query)
+        output = cur.fetchone()
+        min_id = output[0]
+        print(min_id)
+        if min_id < max_id:
+            for i in range(min_id,max_id,1000):
+                second_value=i+999
+                query=(f"update audit_activity set updated_at=ADDTIME(created_at,3) where updated_at is null and id BETWEEN {i} and {second_value};")
+                print(query)
+                cur.execute(query)
+                conn.commit()
+                time.sleep(0.3)
 
-
-    conn.close()
+        conn.close()
+    except Exception as E:
+        print(E)
 
 if __name__ == "__main__":
     mysqlconnect()
